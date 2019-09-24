3c220bc63fc0129529c000a7c15d674745970b28
lawyso <lawrence.owuor@gmail.com>
2019-09-24 16:30:34 +0300

    second day commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SlimLoadingBarModule } from "ng2-slim-loading-bar";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AdminModule } from "./admin/admin.module";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD6pP62qE0V9pmC-lBbfjE5Use-ibjEh68",
+  authDomain: "my-portfolio-cfd10.firebaseapp.com",
+  databaseURL: "https://my-portfolio-cfd10.firebaseio.com",
+  projectId: "my-portfolio-cfd10",
+  storageBucket: "",
+  messagingSenderId: "1092769629911",
+  appId: "1:1092769629911:web:cb9bd34af4eab2e30c1292"
+};
 
 @NgModule({
   declarations: [
@@ -20,9 +34,13 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    AdminModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
